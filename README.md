f2e9609ae7f9cbcd6bf8d828e8995445ac874469
Siyanda Mvunyiswa <steeroy10@gmail.com>
2022-09-06 23:14:02 +0200

    Navigation Bar

---
+++ README.md
@@ -0,0 +1,7 @@
+# Runtime Terror
+
+# Creating Main Page
+
+1. Add Logo on the upper left
+2. Add the two buttons on the right
+3. Add linear gradient as background
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import "./App.css";
+import logo from "./assets/runtime_terror_logo.png";
+import Button from "react-bootstrap/Button";
+import { Icon } from "@iconify/react";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hey there</h1>
+    <div>
+      <header>
+        <div className="nav-contents">
+          <div className="logo-box">
+            <a href="/">
+              <img src={logo} alt="Logo" />
+            </a>
+          </div>
+          <div className="buttons">
+            <Button className="order_history">
+              <Icon icon="icon-park-outline:transaction-order" />
+              <span>Order History</span>
+            </Button>
+            <Button className="view_profile">
+              <Icon icon="ant-design:user-outlined" />
+              <span>View Profile</span>
+            </Button>
+          </div>
+        </div>
       </header>
     </div>
   );
