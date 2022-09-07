20a0c5c1588592106173e6fa13e5ddf0b2296066
Siyanda Mvunyiswa <steeroy10@gmail.com>
2022-09-07 04:28:12 +0200

    Movie Promotion Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,17 @@ import logo from "./assets/runtime_terror_logo.png";
 import Button from "react-bootstrap/Button";
 import { Icon } from "@iconify/react";
 import data from "./data";
+import Thortitle from "./assets/Thor_Love_and_Thunder_Title.png";
+
+const promotedMovie = data.movies.find((movie) => {
+  return movie.title === "Thor: Love and Thunder";
+});
 
 function App() {
   return (
     <div>
-      <header>
+      <nav>
+        {/* This is the navigation bar with the logo and the 2 profile and order history buttons */}
         <div className="nav-contents">
           <div className="logo-box">
             <a href="/">
@@ -25,7 +31,33 @@ function App() {
             </Button>
           </div>
         </div>
-      </header>
+      </nav>
+
+      {/* This section is for the promoted movie, which is Thor: Love and Thunder */}
+      <section>
+        {/* This the image box */}
+        <div className="promoted_movie_image">
+          <img src={promotedMovie.image} alt={promotedMovie.title} />
+        </div>
+
+        {/* This is the content box */}
+        <div className="promoted_movie_contents">
+          <h1 className="now_showing">Now Showing</h1>
+          <div className="thortitle_box">
+            <img src={Thortitle} alt={promotedMovie.title} />
+          </div>
+          <div className="booking_buttons">
+            <Button className="book_movie">
+              <Icon icon="tabler:hand-click" />
+              <span>Book Movie</span>
+            </Button>
+            <Button className="view_movie">
+              <Icon icon="mdi:magnify-plus-outline" />
+              <span>View Movie</span>
+            </Button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
